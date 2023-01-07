d24aaf27d6febdaaee50bba2bcecc404a65a7272
Hidden-Virus <70848309+hiddenvirus1@users.noreply.github.com>
2023-01-08 01:19:55 +0500

    Update links.py

--- links.py
+++ links.py
@@ -11,9 +11,23 @@ print ("\033[95mFB/sharifansari00")
 print ("")
 
 # Prompt the user to enter the path to the file
-print ("\033[1;90m >> Type example.txt\n")
+print ("\033[1;90m >> Type example.txt\033[0m\n")
 path = input("\033[1;92m[$] Enter the path to the file containing the links: \033[0m")
+print("")
 path = os.path.expanduser(path)
+
+# How many Links in your list
+try:
+    # Open the file with the links
+    with open(path, 'r') as f:
+        # Read the file line by line
+        line_num = 0
+        for line in f:
+            line_num += 1
+    print("\033[1;93m  >> This file has {} lines.\033[0m\n".format(line_num))
+except FileNotFoundError:
+    print("\033[1;91m  >> Error: File not found.\033[0m\n")
+    exit()
 
 # Prompt the user to enter the starting line of the links
 start_line = input("\033[1;92m[$] Enter the starting line of the links you want to open: \033[0m")
@@ -34,5 +48,6 @@ with open(path, 'r') as f:
             # Open the link in Firefox
             # If you want to open links in Chrome, rename Firefox to Google Chrome.
             webbrowser.get('firefox').open(line)
-
-print ("\033[1;93mLinks opened successfully!")
+            
+print("")
+print ("\033[1;93m  >> Links opened successfully!\033[0m\n")
